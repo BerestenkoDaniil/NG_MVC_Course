33de7efc8f59b4a9e5ca7561f3b7b417225483fc
Daniil Berestenko <berestenkodaniilki212@gmail.com>
2022-09-23 12:10:13 +0300

    Created Data file with users Logins and Passwords

---
+++ MVC/UserData.cs
@@ -0,0 +1,19 @@
+﻿using MVC.Models;
+using System.Collections.Generic;
+
+namespace MVC
+{
+    public static class UserData
+    {
+        public static List<UserModel> getData()
+        {
+            return new List<UserModel>()
+            {
+                new UserModel() { Login="Danya",Password="qwerty1624",IsAdmin=true },
+                new UserModel() { Login="YAN",Password="228aboba",IsAdmin=true },
+                new UserModel() { Login="ANYA",Password="1834",IsAdmin=false },
+                new UserModel() { Login="Andrei",Password="hlop",IsAdmin=false }
+            };
+        }
+    }
+}
