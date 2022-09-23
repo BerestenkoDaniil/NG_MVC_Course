226b1637350fe5420e56b9a77cdd56fcacb8c9b1
Daniil Berestenko <berestenkodaniilki212@gmail.com>
2022-09-23 12:07:36 +0300

    Set up MVC Controllers

---
+++ MVC/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+        public IActionResult Check(UserModel model)
+        {
+            if (model.Login == null || model.Password == null) 
+            { 
+                return RedirectToAction("Index"); 
+            }
+            foreach(var record in UserData.getData())
+            {
+                if (record.Login == model.Login && record.Password == model.Password)
+                {
+                    model.IsAdmin = record.IsAdmin;
+                    model.IsLogged = true;
+                    break;
+                }
+            }
+            return View("Check",model);
+        }
+    }
+}
