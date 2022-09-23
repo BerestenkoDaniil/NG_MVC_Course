8d2e75f7fc30e386264132834d91199fe85df190
Daniil Berestenko <berestenkodaniilki212@gmail.com>
2022-09-23 12:09:05 +0300

    Set up bassic MVC Models

---
+++ MVC/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.Models
+{
+    public class UserModel
+    {
+       public string Login { get; set; }
+       public string Password { get; set; }
+       public bool IsAdmin { get; set; }
+       public bool IsLogged { get; set; } 
+    }
+}
